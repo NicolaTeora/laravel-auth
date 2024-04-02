6b3fcd4fbf7706b0e969fcd332e9a091e9f4029a
NicolaTeora <nikxodia@gmail.com>
2024-04-02 16:49:48 +0200

    feat : fix footer; migration; registration;

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-white">
-  <div class="container py-4">
-    &copy; {{ env('APP_AUTHOR', 'Tiziano Nicolai') }} {{ now()->year }}
-  </div>
+    <div class="container py-4">
+        &copy; {{ env('APP_AUTHOR', 'N.T.') }} {{ now()->year }}
+    </div>
 </footer>
